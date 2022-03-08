07cbc0eecdede48e9e6f0c133c34ca829ba7223c
Criston Mascarenhas <criston2011@gmail.com>
2022-03-09 00:41:33 +0530

    Basic product & categories api

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# MyQart Ecommerce
+
+
+
+## Tech Stack
+
+**Client:** Angular, NGRX
+
+**Server:** Node, Express
+
+**Database**: MongoDB
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/crstnmac/MyQart
+```
+
+Go to the project directory
+
+```bash
+  cd backend
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const api = process.env.API_URL
+const port = process.env.PORT
+const cors = require('cors')
+
+app.use(cors())
+app.options('*', cors())
+
+//middleware
+app.use(express.json())
+app.use(morgan('tiny'))
+
+//Routers
+const categoriesRouter = require('./routers/categories')
+const ordersRouter = require('./routers/orders')
+const productsRouter = require('./routers/products')
+const usersRouter = require('./routers/users')
+
+app.use(`${api}/categories`, categoriesRouter)
+app.use(`${api}/orders`, ordersRouter)
+app.use(`${api}/products`, productsRouter)
+app.use(`${api}/users`, usersRouter)
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Database connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+app.listen(port, () => {
+  console.log(api)
+  console.log(`Server is running on http://localhost:${port}`)
+})

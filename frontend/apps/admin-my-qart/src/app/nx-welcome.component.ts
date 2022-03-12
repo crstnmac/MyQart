f9aa2700b9b80a6b9ed85b7a6056a787ff0d7af4
Criston Mascarenhas <criston2011@gmail.com>
2022-03-12 23:16:56 +0530

    Created admin dashboard , CRUD for categories

--- frontend/apps/admin-my-qart/src/app/nx-welcome.component.ts
+++ frontend/apps/admin-my-qart/src/app/nx-welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core'
 /* eslint-disable */
 
 @Component({
-  selector: 'frontend-nx-welcome',
+  selector: 'admin-nx-welcome',
   template: `
     <!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
